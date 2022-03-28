b23fd383aaa6c0a788e8e04d6a6a269727d1bba9
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-28 18:40:59 +0900

    added first "component". Placeholder img

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home">
     Home
+    <div class="feature-card">
+      <router-link to="/movie/tt0409591">
+        <img src="https://image.tmdb.org/t/p/w500/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg"
+         alt="" class="featured-img">
+         <div class="detail">
+           <h3>Naruto</h3>
+           <p>Description Goes here</p>
+         </div>
+      </router-link>
+    </div>
   </div>
 </template>
 
