87b1b75447e8df1655509c6def4b2d8f95ccffd2
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-28 18:44:48 +0900

    added style but img doesnt work

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,20 +3,58 @@
     Home
     <div class="feature-card">
       <router-link to="/movie/tt0409591">
-        <img src="https://image.tmdb.org/t/p/w500/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg"
-         alt="" class="featured-img">
-         <div class="detail">
-           <h3>Naruto</h3>
-           <p>Description Goes here</p>
-         </div>
+        <img
+          src="https://image.tmdb.org/t/p/w500/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg"
+          alt=""
+          class="featured-img"
+        />
+        <div class="detail">
+          <h3>Naruto</h3>
+          <p>Description Goes here</p>
+        </div>
       </router-link>
     </div>
   </div>
 </template>
 
 <script>
-
-export default {
-
-}
+export default {};
 </script>
+
+<style lang="scss">
+.home {
+  .feature-card {
+    position: relative;
+
+    .feratured-img {
+      display: block;
+      width: 100%;
+      height: 300px;
+      object-fit: cover;
+      z-index: 0;
+    }
+
+    .detail {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      background-color: rgba(0, 0, 0, 0.5);
+      padding: 16px;
+      z-index: 1;
+
+      h3 {
+        color: #fff;
+        font-size: 1.5rem;
+        margin-bottom: 8px;
+      }
+
+      p {
+        color: #fff;
+        font-size: 1rem;
+      }
+
+    }
+  }
+}
+</style>
