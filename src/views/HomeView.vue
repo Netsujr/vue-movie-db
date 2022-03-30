fe7d83d724a51100af3390903987b47c87b4f05b
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-30 10:58:29 +0900

    fixed movie card style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,11 +17,13 @@
     <div class="movies-list">
       <div class="movie" v-for="movie in movies" :key="movie.imdbID">
         <router-link :to="'/movie/' + movie.imdbID" class="movie-link">
-          <img :src="movie.Poster" alt="moviePoster" class="movie-img" />
-          <p class="type">{{ movie.Type }}</p>
-          <div class="movie-detail">
+          <div class="product-image">
+            <img :src="movie.Poster" alt="Movie Poster" />
+            <div class="type">{{ movie.Type }}</div>
+          </div>
+          <div class="detail">
+            <p class="year">{{ movie.Year }}</p>
             <h3>{{ movie.Title }}</h3>
-            <p>{{ movie.Year }}</p>
           </div>
         </router-link>
       </div>
@@ -112,6 +114,56 @@ export default {
       background-color: #4caf50;
       color: #fff;
       cursor: pointer;
+    }
+  }
+
+  .movies-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px 8px;
+    .movie {
+      max-width: 50%;
+      flex: 1 1 50%;
+      padding: 16px 8px;
+      .movie-link {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        .product-image {
+          position: relative;
+          display: block;
+          img {
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+          .type {
+            position: absolute;
+            padding: 8px 16px;
+            background-color: #42b883;
+            color: #fff;
+            bottom: 16px;
+            left: 0px;
+            text-transform: capitalize;
+          }
+        }
+        .detail {
+          background-color: #496583;
+          padding: 16px 8px;
+          flex: 1 1 100%;
+          border-radius: 0px 0px 8px 8px;
+          .year {
+            color: #aaa;
+            font-size: 14px;
+          }
+          h3 {
+            color: #fff;
+            font-weight: 600;
+            font-size: 12px;
+          }
+        }
+      }
     }
   }
 }
