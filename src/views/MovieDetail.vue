a25056b88f94f4aa32004a7f5003033f035fc355
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-31 15:52:49 +0900

    api works, but not routing to detail page

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="movie-detail">
-    Movie Detail {{ $route.params.id }}
+    <h2>{{movie.Title}}</h2>
   </div>
 </template>
 
 <script>
+import { ref, onBeforeMount } from 'vue';
+import { useRoute } from 'vue-router';
+import env from '@/env';
+
 export default {
+  setup () {
+    const movie = ref({});
+    const route = useRoute();
+
+    onBeforeMount(() => {
+      fetch(`https://omdbapi.com/?apikey=${env.apiKey}&i=${route.params.id}&plot=full`)
+        .then(res => res.json())
+        .then(data => {
+          movie.value = data;
+        });
+    });
+
+    return {
+      movie,
+      route
+    };
+  }
 
 }
 </script>
